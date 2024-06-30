7c151494cd4afa5a57ed3d02958d6adb55a4cf74
WninaE <wninae@unjbg.edu.pe>
2024-06-30 15:10:29 -0500

    Añadir Proyecto: Incluye funciones.cpp, funciones.h, main.cpp y Proyecto.dev

---
+++ Proyecto/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "funciones.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+
+int main(){
+	int a=10, b=5;
+	cout<<"Suma: "<<suma(a, b)<<endl;
+	cout<<"Resta: "<<resta(a, b)<<endl;
+	return 0;
+}
