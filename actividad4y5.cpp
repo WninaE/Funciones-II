5d7142707a62cee755402949a9b48aee4336d48e
WninaE <wninae@unjbg.edu.pe>
2024-06-30 15:38:03 -0500

    Adding actividad5.cpp: Update of actividad4.cpp

--- actividad4y5.cpp
+++ actividad4y5.cpp
@@ -1,27 +1,34 @@
-//actividad4.cpp
-//4. Cálculo de raíces de una ecuación cuadrática
+//actividad5.cpp
+//5. Función calcular raíces
 //Nina Estuco, William André
 #include <iostream>
 #include <cmath>
 using namespace std;
 
+void raices(int m, int n, int o);
+
 int main(){
-    int a, b, c, d, r1, r2, r;
+    int a, b, c;
     cout<<"Ingrese los coeficientes: ";
     cin>>a>>b>>c;
     cout<<"Las raices de la ecuacion cuadratica son ";
-    d=pow(b,2)-(4*a*c);
+    raices(a, b, c);
+    return 0;
+}
+
+void raices(int m, int n, int o){
+    float d, r1, r2, r;
+    d=pow(n,2)-(4*m*o);
     if(d>0){
         cout<<"reales, tiene dos soluciones.\n";
-        r1=(-b+sqrt(d))/(2*a);
-        r2=(-b-sqrt(d))/(2*a);
+        r1=(-n+sqrt(d))/(2*m);
+        r2=(-n-sqrt(d))/(2*m);
         cout<<"Las raices son "<<r1<<" y "<<r2<<endl;
     } else if(d==0){
-        cout<<"una unica solucion real\n";
-        r=-b/(2*a);
+        cout<<"una unica solucion real.\n";
+        r=-n/(2*m);
         cout<<"La raiz es "<<r<<endl;
     } else{
         cout<<"imaginarias, no tiene soluciones reales."<<endl;
     }
-    return 0;
 }
